b498e83783c828633ae9ec2927bb8c5a437a0167
nwiedel <164350832+nwiedel@users.noreply.github.com>
2025-08-29 23:24:02 +0200

    2.1. GameConfig

---
+++ core/src/main/java/com/myspaceshooter/configuration/GameConfig.java
@@ -0,0 +1,22 @@
+package com.myspaceshooter.configuration;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class GameConfig {
+
+    public final static Color CORNFLOWER_BLUE = new Color(0.388f, 0.584f, 0.933f, 0.0f);
+
+    public static final float WIDTH = 1600f;
+    public static final float HEIGHT = 960f;
+
+    public static final float HUD_WIDTH = 1600f;
+    public static final float HUD_HEIGHT = 960f;
+
+    public static final float WORLD_WIDTH = 20.0f;
+    public static final float WORLD_HEIGHT = 12.0f;
+
+    public static final float WORLD_CENTER_X = WORLD_WIDTH / 2f;
+    public static final float WORLD_CENTER_Y = WORLD_HEIGHT / 2f;
+
+    private GameConfig(){}
+}
--- lwjgl2/src/main/java/com/myspaceshooter/lwjgl2/Lwjgl2Launcher.java
+++ lwjgl2/src/main/java/com/myspaceshooter/lwjgl2/Lwjgl2Launcher.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.myspaceshooter.SpaceShooterGame;
+import com.myspaceshooter.configuration.GameConfig;
 
 /** Launches the desktop (LWJGL2) application. */
 public class Lwjgl2Launcher {
@@ -18,8 +19,8 @@ public class Lwjgl2Launcher {
     private static LwjglApplicationConfiguration getDefaultConfiguration() {
         LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
         configuration.title = "Space Shooter Prototype";
-        configuration.width = 640;
-        configuration.height = 480;
+        configuration.width = (int)GameConfig.WIDTH;
+        configuration.height = (int) GameConfig.HEIGHT;
         //// This prevents a confusing error that would appear after exiting normally.
         configuration.forceExit = false;
 
